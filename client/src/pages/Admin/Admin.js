fa6f4f50e965e503607e7b4da5314360340b1dff
elspetha5 <elspetha@gmail.com>
2018-06-29 15:18:37 -0700

    page folders set up

---
+++ client/src/pages/Admin/Admin.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./admin.css";
+
+class Admin extends Component {
+    state = {
+
+    };
+
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+};
+
+export default Admin;
---
+++ client/src/pages/Resaurant/Resaurant.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./restaurant.css";
+
+const Restaurant = props => (
+    <div>
+
+    </div>
+);
+
+export default Restaurant;
