40395f7fccf22d6e512eb0ac212a2e9d86e8ecaf
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-07-03 19:21:52 -0700

    homepage looks good

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,34 +5,34 @@ import "./home.css";
 const Home = () => (
 <div>
     	<div className="jumbotron">
-    		<h1>Welcome to Picky Eats!</h1>
+    		<h1 id="mainTitle">Welcome to Picky Eats!</h1>
 
     		<h3 id="subtitle">Start searching for your next dish by entering the restaurants name or what you're in the mood for!</h3>
 
-    		<input type="text" placeholder="Search by Restaurant...">
+    		<input type="text" placeholder="Search by Restaurant..."/>
     		<button className="btn">Search</button>
 
-    		<br>
+    		<div></div>
     	
     		
-    	<div class="dropdown">
-  			<button class="dropbtn">I'm in the mood for</button>
+    	<div className="dropdown">
+  			<button className="dropbtn">I'm in the mood for</button>
   				<div className="dropdown-content">
-    				<a href="#">Chinese</a>
-    				<a href="#">Mexican</a>
-    				<a href="#">Korean</a>
-    				<a href="#">American</a>
-    				<a href="#">Steakhouses</a>
-    				<a href="#">Italian</a>
-    				<a href="#">Seafood</a>
-    				<a href="#">Breakfast</a>
-    				<a href="#">Pizza</a>
-    				<a href="#">Burgers</a>
-    				<a href="#">Thai</a>
-    				<a href="#">Japanese</a>
-    				<a href="#">Vietnamese</a>
-    				<a href="#">Sandwiches</a>
-    				<a href="#">Sushi Bars</a>
+    				<a href="">Chinese</a>
+    				<a href="">Mexican</a>
+    				<a href="">Korean</a>
+    				<a href="">American</a>
+    				<a href="">Steakhouses</a>
+    				<a href="">Italian</a>
+    				<a href="">Seafood</a>
+    				<a href="">Breakfast</a>
+    				<a href="">Pizza</a>
+    				<a href="">Burgers</a>
+    				<a href="">Thai</a>
+    				<a href="">Japanese</a>
+    				<a href="">Vietnamese</a>
+    				<a href="">Sandwiches</a>
+    				<a href="">Sushi Bars</a>
   				</div>
 		</div>
 		
@@ -43,7 +43,7 @@ const Home = () => (
  		<h1 id="fTitle">Featured Restaurants</h1>
 
  		<div className="card">
-  			<img src="images/bar.jpg" alt="restaurant" style="width:100%">
+  			<img src="" alt="restaurant"/>
   				<div className="restaurantinfo">
     				<h4><b>Restaurant Name</b></h4> 
     					<p>Restaurant info here</p> 
@@ -52,7 +52,7 @@ const Home = () => (
 		</div>
 
 		<div className="card">
-  			<img src="images/table.jpg" alt="restaurant" style="width:100%">
+  			<img src="" alt="restaurant"/>
   				<div className="restaurantinfo">
     				<h4><b>Restaurant Name</b></h4> 
     					<p>Restaurant info here</p> 
@@ -61,7 +61,7 @@ const Home = () => (
 		</div>
 
 		<div className="card">
-  			<img src="images/jumboImage.jpg" alt="restaurant" style="width:100%">
+  			<img src="" alt="restaurant"/>
   				<div className="restaurantinfo">
     				<h4><b>Restaurant Name</b></h4> 
     					<p>Restaurant info here</p>
