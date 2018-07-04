6364d083c07632005b054ea4b38e1ae8aadf4011
elspetha5 <elspetha@gmail.com>
2018-07-03 19:53:58 -0700

    ResSearch added

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./nav.css";
 
 const Nav = () => (
-    <div>
-        <div className="navbar">
-		<ul>
-  			<li><a href="#home">Login</a></li>
-  			<li><a className="active" id="logo" href="#about">Picky Eats</a></li>
-		</ul>
+	<div>
+		<div className="navbar">
+			<ul>
+				<li><a href="#home">Login</a></li>
+				<li><a className="active" id="logo" href="#about">Picky Eats</a></li>
+			</ul>
+		</div>
 	</div>
-    </div>
 );
 
 export default Nav;
