cff1587e3233368e5f916642c392a88c6b858e87
nvidya.karthik <nvidya.karthik@gmail.com>
2018-07-06 16:28:26 -0700

    added menu route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
-const loginRoutes = require("./login");
-const restaurantRoutes=require("./restaurant");
+const loginRoute = require("./login");
+const restaurantRoute=require("./restaurant");
+const menuRoute=require("./menu");
 
 // user login routes
-router.use("/user", loginRoutes);
+router.use("/user", loginRoute);
 // user restaurant routes
-router.use("/restaurant", restaurantRoutes);
+router.use("/restaurant", restaurantRoute);
+router.use("/menu",menuRoute);
 
 module.exports = router;
