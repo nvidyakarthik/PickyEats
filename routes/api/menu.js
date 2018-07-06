947bcd34de20a2497ced6ece2c2d204a8ffdac56
nvidya.karthik <nvidya.karthik@gmail.com>
2018-07-06 16:29:16 -0700

    modified menu schema to hold comments

--- routes/api/menu.js
+++ routes/api/menu.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 const menuController = require("../../controllers/menuController");
 
 //for the route api/menu/comment/:id
-/* router.route("/comment/:id")
+router.route("/comment/:id")
 .post(menuController.createComment)
-.get(menuController.findAllComments); */
+.get(menuController.findAllComments); 
 
 //for the route api/menu/save
 router.route("/save/:id")
 .post(menuController.createMenu);
+
+//for the route api/menu/save
+router.route("/edit/:id")
+.put(menuController.updateMenu);
 module.exports=router;
