2c021890f94d46c8c646859341b1bf5e6677d47e
nvidya.karthik <nvidya.karthik@gmail.com>
2018-07-10 16:14:20 -0700

    modified Nav component for authentication

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom'
 import "./nav.css";
 
-const Nav = () => (
-	<div>
-		<div className="navbar">
-			<ul>
-				<li><a href="#home">Login</a></li>
-				<li><a className="active" id="logo" href="/">Picky Eats</a></li>
-			</ul>
-		</div>
-	</div>
-);
-
+const Nav = props => {
+	if (props.loggedIn) {
+		return (
+			
+				<ul className="nav">
+					<li>
+						<Link to="/" id="logo" className="active">
+							Picky Eats
+						</Link>
+						
+					</li>
+					<li>
+						<a href="#">{props.user.firstName}</a>
+					</li>
+					<li>
+						<Link to="/logout" className="nav-link" onClick={props._logout}>
+							Logout
+						</Link>
+					</li>
+				</ul>
+			
+		);
+	} else {
+		return (
+			
+				<ul className="nav">
+					<li>
+						<Link to="/" className="active" id="logo">
+							Picky Eats
+						</Link> 
+					</li>
+					<li>
+						<Link to="/login">
+							login
+						</Link>
+					</li>
+					<li>
+						<Link to="/signup">
+							sign up
+						</Link>
+					</li>
+				</ul>
+			
+		);
+	}
+}
 export default Nav;
