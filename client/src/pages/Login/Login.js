773bc8ede7441e116280ca93de1a5cfc1a8f134b
nvidya.karthik <nvidya.karthik@gmail.com>
2018-07-10 16:16:22 -0700

    splited the login page for authentication

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import Container from "../../components/Container";
 import "./login.css";
+import API from "../../utils/API";
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
-    state = {
-        firstName: "",
-        lastName: "",
-        username: "",
-        password: "",
-        confirmPass: "",
-        resOwner: false
-    };
+    constructor() {
+		super()
+		this.state = {
+			email: "",
+            password: "" ,
+			redirectTo: null
+		}
+		// this.googleSignin = this.googleSignin.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleInputChange = this.handleInputChange.bind(this)
+	};
+    
 
     handleInputChange = event => {
         const { name, value } = event.target;
@@ -19,53 +25,29 @@ class Login extends Component {
         });
     };
 
+    handleSubmit=(event)=>{
+        
+        event.preventDefault();
+        this.props._login(this.state.email, this.state.password)
+		this.setState({
+			redirectTo: '/'
+		});
+		
+    }
+
     render() {
+        if (this.state.redirectTo) {
+			return <Redirect to={{ pathname: this.state.redirectTo }} />
+		} else {
         return (
             <Container>
-                <div className="half">
-                    <h3 className="title">Sign Up</h3>
-
-                    <form>
-                        <input
-                            name="firstName"
-                            placeholder="First Name (required)"
-                            value={this.state.firstName}
-                            onChange={this.handleInputChange}
-                        />
-                        <input
-                            name="lastName"
-                            placeholder="Last Name (required)"
-                            value={this.state.lastName}
-                            onChange={this.handleInputChange}
-                        />
-                        <input
-                            name="userName"
-                            placeholder="Username (required)"
-                            value={this.state.username}
-                            onChange={this.handleInputChange}
-                        />
-                        <input
-                            name="password"
-                            placeholder="Password (required)"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                        />
-                        <input
-                            name="confirmPass"
-                            placeholder="Confirm Password (required)"
-                            value={this.state.confirmPass}
-                            onChange={this.handleInputChange}
-                        />
-                        <button id="createAcc">Create Account</button>
-                    </form>
-                </div>
-
+                
                 <div className="half">
                     <h3 className="title">Sign In</h3>
 
                     <input
-                        name="userName"
-                        placeholder="Username (required)"
+                        name="email"
+                        placeholder="Email (required)"
                         value={this.state.username}
                         onChange={this.handleInputChange}
                     />
@@ -75,11 +57,12 @@ class Login extends Component {
                         value={this.state.password}
                         onChange={this.handleInputChange}
                     />
-                    <button id="signIn">Sign In</button>
+                    <button id="signIn" onClick={this.handleSubmit}>Sign In</button>
                 </div>
             </Container>
         )
     }
+}
 };
 
 export default Login;
