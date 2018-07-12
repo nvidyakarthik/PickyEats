e261bd037b1a41b876e5d43dba1cf210ffb8c4b3
nvidya.karthik <nvidya.karthik@gmail.com>
2018-07-12 11:45:54 -0700

    moved updateRestMenu to restcontroller.js

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -2,30 +2,27 @@ const db = require("../models");
 const axios = require("axios");
 
 module.exports = {
-
+   
     createMenu: function (req, res) {
 
-        db.Menu
-            .create(req.body)
-            .then(dbMenu => {
-                console.log("params" + req.params.id);
-                console.log("dbMenu" + dbMenu._id);
-                return db.Restaurant.findOneAndUpdate({ _id: req.params.id },
-                    { $push: { menus: dbMenu._id } },
-                    { new: true });
+      db.Menu
+          .create(req.body)
+          .then(dbMenu => {
+              console.log("params" + req.params.id);
+              console.log("dbMenu" + dbMenu._id);
+              /* return db.Restaurant.findOneAndUpdate({ _id: req.params.id },
+                  { $push: { menus: dbMenu._id } },
+                  { new: true }).populate("menus"); */
+                  res.json(dbMenu);
 
-            })
-            .then(dbRestaurant => {
+          })
+          
+          .catch(err => {
+              console.log(err);
+              res.status(422).json(err);
 
-                res.json(dbRestaurant);
-
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(422).json(err);
-
-            });
-    },
+          });
+  },
     createComment: function(req, res) {
     console.log(req.body);
     db.Comment
