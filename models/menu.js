4bc4f76226fc7273b30f3d7f5a0c836b33722a1a
nvidya.karthik <nvidya.karthik@gmail.com>
2018-07-18 19:52:16 -0700

    added restaurantId for loading skipped menu fields

--- models/menu.js
+++ models/menu.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const menuSchema = new Schema({
+  restaurantId:{type:Schema.Types.ObjectId,ref:'Restaurant'},
   dishName: { type: String, required: true ,trim:true},
   description: { type: String,trim:true},
   price: {type:Number,required:true},
   menutype:{type:String,required:true,trim:true},
   rating:{type:Number,default:0},
-  comments:[{ type: Schema.Types.ObjectId, ref: 'Comment' }]      
+  comments:[{ type: Schema.Types.ObjectId, ref: 'Comment' }]        
 });
 
 const Menu = mongoose.model("Menu", menuSchema);
